c49c008f16ec7f8ea9e09a813c42433ab4d9dcb1
Rafal Nater <naterek@gmail.com>
2016-05-19 19:11:11 +0200

    Add recipe CRUD abilities

---
+++ src/AppBundle/Controller/RecipeController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Recipe;
+use AppBundle\Form\RecipeType;
+
+/**
+ * Recipe controller.
+ *
+ * @Route("/recipe")
+ */
+class RecipeController extends Controller
+{
+    /**
+     * Lists all Recipe entities.
+     *
+     * @Route("/", name="recipe_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $recipes = $em->getRepository('AppBundle:Recipe')->findAll();
+
+        return $this->render('recipe/index.html.twig', array(
+            'recipes' => $recipes,
+        ));
+    }
+
+    /**
+     * Creates a new Recipe entity.
+     *
+     * @Route("/new", name="recipe_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm('AppBundle\Form\RecipeType', $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($recipe);
+            $em->flush();
+
+            return $this->redirectToRoute('recipe_show', array('id' => $recipe->getId()));
+        }
+
+        return $this->render('recipe/new.html.twig', array(
+            'recipe' => $recipe,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Recipe entity.
+     *
+     * @Route("/{id}", name="recipe_show")
+     * @Method("GET")
+     */
+    public function showAction(Recipe $recipe)
+    {
+        $deleteForm = $this->createDeleteForm($recipe);
+
+        return $this->render('recipe/show.html.twig', array(
+            'recipe' => $recipe,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Recipe entity.
+     *
+     * @Route("/{id}/edit", name="recipe_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Recipe $recipe)
+    {
+        $deleteForm = $this->createDeleteForm($recipe);
+        $editForm = $this->createForm('AppBundle\Form\RecipeType', $recipe);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($recipe);
+            $em->flush();
+
+            return $this->redirectToRoute('recipe_edit', array('id' => $recipe->getId()));
+        }
+
+        return $this->render('recipe/edit.html.twig', array(
+            'recipe' => $recipe,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Recipe entity.
+     *
+     * @Route("/{id}", name="recipe_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Recipe $recipe)
+    {
+        $form = $this->createDeleteForm($recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($recipe);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('recipe_index');
+    }
+
+    /**
+     * Creates a form to delete a Recipe entity.
+     *
+     * @param Recipe $recipe The Recipe entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Recipe $recipe)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('recipe_delete', array('id' => $recipe->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Recipe.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Recipe
+ *
+ * @ORM\Table(name="recipe")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RecipeRepository")
+ * 
+ */
+class Recipe
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Recipe
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Recipe
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+}
